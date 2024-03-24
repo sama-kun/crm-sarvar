2a1dd101762e81adddb083bcad4476cfc7402824
sama-kun <samgar.robot@gmail.com>
2024-03-24 06:50:20 +0500

    delete migration

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,30 +1,33 @@
-import { DataSource, DataSourceOptions } from 'typeorm';
-import * as dotenv from 'dotenv';
-import * as fs from 'fs-extra';
+import { DataSource, DataSourceOptions } from "typeorm";
+import * as dotenv from "dotenv";
+import * as fs from "fs-extra";
 dotenv.config();
 console.log(
-  `Uses postgres database ${process.env.POSTGRES_NAME} at ${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}`,
+  `Uses postgres database ${process.env.POSTGRES_NAME} at ${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}`
 );
 
 export const appDataSource = new DataSource({
-  type: 'postgres',
+  type: "postgres",
   host: process.env.POSTGRES_HOST,
-  port: parseInt(process.env.POSTGRES_PORT || '5432') || 5432,
+  port: parseInt(process.env.POSTGRES_PORT || "5432") || 5432,
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_NAME,
   autoLoadEntities: true,
-  entities: ['src/database/entities/**/*.entity{.ts,.js}'],
-	synchronize: true,
-  migrationsRun: process.env.NODE_ENV !== 'development',
-	migrations: ['src/database/migrations/**/*.ts'],
-	subscribers: ['src/database/subscribers/**/*.subscriber{.ts,.js}'],
-	cli: {
-		entitiesDir: 'src/modules/',
-		migrationsDir: 'src/database/migrations/',
-		seedsDir: 'src/database/seeds/',
-	},
-  ssl: process.env.DB_SSl || false,
+  entities: ["src/database/entities/**/*.entity{.ts,.js}"],
+  synchronize: true,
+  migrationsRun: process.env.NODE_ENV !== "development",
+  migrations: ["src/database/migrations/**/*.ts"],
+  subscribers: ["src/database/subscribers/**/*.subscriber{.ts,.js}"],
+  cli: {
+    entitiesDir: "src/modules/",
+    migrationsDir: "src/database/migrations/",
+    seedsDir: "src/database/seeds/",
+  },
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
   // extra: {
   //   ssl: {
   //     ca: fs.readFileSync('./cer_ksi.crt'),
